192833c8ade13aa4f159f29ce017aff329ad8728
Andrey Frolov <inham130@gmail.com>
2021-10-21 18:43:19 +0300

    fixed node server to return index.html

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express();
 const PORT = 3000;
 app.use(express.static(`${__dirname}/dist`));
 
-app.listen(PORT, function () {
+app.get('*', function(req, res) {
+    res.sendFile(__dirname + '/dist/index.html');
+});
+
+app.listen(PORT, function() {
   console.log(`Example app listening on port ${PORT}!`);
-}); 
+});
